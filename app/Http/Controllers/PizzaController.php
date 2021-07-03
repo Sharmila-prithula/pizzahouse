f07bd2e2fbd194c50034d25dc59553e6c4a69164
Sharmila-prithula <sharmilarahmanprithula@gmail.com>
2021-07-03 11:53:42 +0600

    fifth commit

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -10,14 +10,18 @@ class PizzaController extends Controller
 {
     public function index(){
         #$pizzas=[['name'=>'Hawaiian', 'type'=>'cheesy', 'base'=>'thin crust'],['name'=>'Italian', 'type'=>'sausage', 'base'=>'soft crust']];
-        #$pizzas= Pizza::all();
+        $pizzas= Pizza::all();
         #$pizzas= Pizza::orderby('name','desc')->get();
         #$pizzas= Pizza::where('type','Italian')->get();
-        $pizzas= Pizza::latest()->get();
-        return view('pizzas', ['pizzas'=>$pizzas]);
+        #$pizzas= Pizza::latest()->get();
+        return view('pizzas.index', ['pizzas'=>$pizzas]);
     }
 
     public function show($id){
-        return view('details', ['id'=>$id]);
+        return view('pizzas.show', ['id'=>$id]);
+    }
+
+    public function create(){
+        return view('pizzas.create');
     }
 }
